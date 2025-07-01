77cae39befde3ba76b50c4b2478f15018106f5ca
Tarek Abdennadher <tarek.abdennadher@primatec.lan>
2025-07-01 15:35:45 +0100

    Add GitHub Actions workflow for Docker build and push2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Dockerized Flask App!!!"
+    return "Hello from Dockerized Flask AApp!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
